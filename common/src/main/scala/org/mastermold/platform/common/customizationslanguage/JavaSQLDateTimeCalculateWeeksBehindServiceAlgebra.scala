518c60a9cdbbcee176c379fa3c72e4db162f19ba
Nick <Odumo>
2020-06-23 01:27:10 -0400

    Working on core project

---
+++ common/src/main/scala/org/mastermold/platform/common/customizationslanguage/JavaSQLDateTimeCalculateWeeksBehindServiceAlgebra.scala
@@ -0,0 +1,5 @@
+package org.mastermold.platform.common.customizationslanguage
+
+trait JavaSQLDateTimeCalculateWeeksBehindServiceAlgebra {
+
+}
---
+++ common/src/main/scala/org/mastermold/platform/common/customizationslanguage/interpreters/package.scala
@@ -0,0 +1,5 @@
+package org.mastermold.platform.common.customizationslanguage
+
+package object interpreters {
+
+}
