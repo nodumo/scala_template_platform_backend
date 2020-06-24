74846aa28e16f6e9995a402dc284d0faf84334ae
Nick <Odumo>
2020-06-23 23:48:28 -0400

    Update: Application integration script definition

--- application/src/main/scala/org/mastermold/platform/application/definitions/Result.scala
+++ application/src/main/scala/org/mastermold/platform/application/definitions/Result.scala
@@ -1,9 +1,10 @@
 package org.mastermold.platform.application.definitions
 
 /**
- * Application payload.
+ * Application result.
  *
  * @author Nick Odumo (nodumo@nodumo.com)
+ * @tparam Payload Payload
  * @param payload Payload
  */
 final case class Result[Payload](payload: Payload) extends AnyVal
