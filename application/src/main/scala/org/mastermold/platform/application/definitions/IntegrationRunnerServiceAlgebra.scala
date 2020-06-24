bf59babceec89f1a526a344d5530d1e383f9cbb1
Nick <Odumo>
2020-06-23 23:47:37 -0400

    Update: Application integration script definition

--- application/src/main/scala/org/mastermold/platform/application/definitions/IntegrationRunnerServiceAlgebra.scala
+++ application/src/main/scala/org/mastermold/platform/application/definitions/IntegrationRunnerServiceAlgebra.scala
@@ -2,6 +2,6 @@ package org.mastermold.platform.application.definitions
 
 trait IntegrationRunnerServiceAlgebra[F[_], InputEvent <: Event[_], DTO] {
 
-  def runIntegration(integrationService: IntegrationServiceAlgebra[F, InputEvent, DTO])(event: InputEvent): F[DTO]
+  def runIntegration(integrationService: IntegrationServiceAlgebra[F, InputEvent, DTO])(event: InputEvent): F[Result[DTO]]
 
 }
