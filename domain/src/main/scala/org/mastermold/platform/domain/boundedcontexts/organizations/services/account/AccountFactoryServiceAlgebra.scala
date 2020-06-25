2379facf633750f54a37c05fb293098aee2b2afe
Nick <Odumo>
2020-06-24 22:58:21 -0400

    Update: Building up the domain model

---
+++ domain/src/main/scala/org/mastermold/platform/domain/boundedcontexts/organizations/services/account/AccountFactoryServiceAlgebra.scala
@@ -0,0 +1,6 @@
+package org.mastermold.platform.domain.boundedcontexts.organizations.services.account
+
+import org.mastermold.platform.common.softwarepatterns.FactoryServiceEffectfulAlgebra
+
+trait AccountFactoryServiceAlgebra[F[_], Repr, Aggregate] extends FactoryServiceEffectfulAlgebra[F, Repr, Aggregate]
+ 
