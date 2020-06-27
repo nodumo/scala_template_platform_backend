9708b7bef2b9cee78fed1824c6f13bf5b415e436
Nick <Odumo>
2020-06-26 20:52:10 -0400

    Update: Infrastructure scaffolding

--- domain/src/main/scala/org/mastermold/platform/domain/boundedcontexts/organizations/services/account/PasswordFactoryServiceAlgebra.scala
+++ domain/src/main/scala/org/mastermold/platform/domain/boundedcontexts/organizations/services/account/PasswordFactoryServiceAlgebra.scala
@@ -1,5 +1,7 @@
 package org.mastermold.platform.domain.boundedcontexts.organizations.services.account
 
-import org.mastermold.platform.common.softwarepatterns.FactoryServiceEffectfulAlgebra
+import org.mastermold.platform.domain.definitions.{DomainEntity, DomainEntityFactoryServiceEffectfulAlgebra}
 
-trait PasswordFactoryServiceAlgebra[F[_], Repr, Value] extends FactoryServiceEffectfulAlgebra[F, Repr, Value]
+trait PasswordFactoryServiceAlgebra[F[_], Repr, Value <: DomainEntity]
+  extends DomainEntityFactoryServiceEffectfulAlgebra[F, Repr, Value]
+
