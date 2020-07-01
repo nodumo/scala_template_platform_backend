face93a74665ad4c71457a9c9e391395d3c2a87e
Nick <Odumo>
2020-07-01 01:07:20 -0400

    Update: Abstracting over Numeric typeclass for predicates. Added migration updates.

--- common/src/main/scala/org/mastermold/platform/common/calculate/NumericCalculationsAlgebra.scala
+++ common/src/main/scala/org/mastermold/platform/common/calculate/NumericCalculationsAlgebra.scala
@@ -1,5 +1,21 @@
 package org.mastermold.platform.common.calculate
 
-trait NumericCalculationsAlgebra {
+/**
+  * Numeric calculations algebra.
+  *
+  * @author Nick Odumo (nodumo@nodumo.com)
+  * @tparam N Numeric type
+  */
+trait NumericCalculationsAlgebra[N] {
+
+  def incrementBy(original: N, toAdd: N): N
+
+  def incrementByOne(original: N, toAdd: N): N
+
+  def decrementBy(original: N, toAdd: N): N
+
+  def decrementByOne(original: N, toAdd: N): N
+
+  def incrementByOneFloorZero(original: N, toAdd: N): N
 
 }
